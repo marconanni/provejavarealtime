d41528ba97f566c83fbdf0772f53a3b2e2755123
marco.nanni0 <marco.nanni0@2b8956b1-77ff-52be-be7e-b8698f75239f>
2011-05-18 16:05:02 +0000

    Finite gli esperimenti sullo scheduler

--- trunk/SolarisNetBeansProjects/ProveJavaRealTime/src/prova18/SleepingHandler18.java
+++ trunk/SolarisNetBeansProjects/ProveJavaRealTime/src/prova18/SleepingHandler18.java
@@ -39,7 +39,7 @@ public class SleepingHandler18 extends DeadlineMissedHandler {
     @Override
     public void handleAsyncEvent() {
         super.getLog().writeDeadlineMissed(this.getControlledThread().getName());
-        super.getControlledThread().schedulePeriodic();
+        
 
       
                  
@@ -51,8 +51,8 @@ public class SleepingHandler18 extends DeadlineMissedHandler {
             Logger.getLogger(SleepingHandler18.class.getName()).log(Level.SEVERE, null, ex);
         }
                
-            super.getLog().writeGenericMessage(" changing release parameters");
-            super.getControlledThread().setReleaseParameters(new PeriodicParameters(new RelativeTime(200, 0)));
+            super.getLog().writeGenericMessage(" changsupering release parameters");
+            super.getControlledThread().schedulePeriodic();
 
           
          
