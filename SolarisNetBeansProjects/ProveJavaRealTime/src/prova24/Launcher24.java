32cd30172f97500a3ffb15bfad9098e642e78270
marco.nanni0 <marco.nanni0@2b8956b1-77ff-52be-be7e-b8698f75239f>
2011-06-22 08:32:50 +0000

    Fatto l'handler asap stop, funziona ma è da testare ulteriormente. Introdotto anche l'interrumpiblebadthread, cehe non ha bisogno di presentazioni

---
+++ SolarisNetBeansProjects/ProveJavaRealTime/src/prova24/Launcher24.java
@@ -0,0 +1,115 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package prova24;
+
+
+
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.realtime.*;
+import realtimeLibrary.logging.*;
+import realtimeLibrary.schedulables.*;
+import realtimeLibrary.busyWait.BusyWait;
+
+
+/**
+ *
+ * @author root
+ */
+public class Launcher24 extends RealtimeThread {
+
+
+    public static void main (String [] args ){
+
+        Launcher24 launcher = new Launcher24();
+        launcher.setName("launcher");
+        launcher.setPriority(PriorityScheduler.instance().getMaxPriority());
+        launcher.start();
+
+
+        
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println("Laucher: confugurazione in corso");
+
+        // creo il thread che deve occupare una cpu
+        CpuDespotThread despotTrhead = new CpuDespotThread();
+        despotTrhead.setName("despotThread");
+        despotTrhead.setPriority(PriorityScheduler.instance().getNormPriority()+4);
+
+        
+        
+
+        // creo il thread 1:un thread che al primo ciclo ha un'esecuzione di
+        // due cicli e mezzo. Implemento la politica di skip affidandoli
+        // skipPolicyHandler
+        // il thread ha periodo 100 ms ed esecuzione normale di 20
+
+        InterrumpibleBadThread th1 =  new InterrumpibleBadThread();
+        th1.setReleaseParameters(new PeriodicParameters(new RelativeTime(100, 0)));
+        th1.setName("Thread1");
+        th1.setPriority(PriorityScheduler.instance().getNormPriority()+1);
+        th1.setExcecutionTime(10);
+        th1.setBadExcecutionTime(350);
+        th1.setBadIteration(1);
+        th1.setNumberOfIterations(7);
+
+        ASAPStopThresholdPolicyHandler handler = new ASAPStopThresholdPolicyHandler(th1, PriorityScheduler.instance().getNormPriority()+2, "Handler",2);
+        th1.setDeadlineMissedHandler(handler);
+
+
+        
+
+       
+
+
+
+        System.out.println("Laucher: confugurazione BusyWait");
+        BusyWait busyWait = BusyWait.getInstance();
+        busyWait.initialize(1000, 3000);
+
+//        System.out.println("Laucher: Lancio tra 10 secondi!");
+//        try {
+//            Thread.sleep(10000);
+//        } catch (InterruptedException ex) {
+//            Logger.getLogger(Launcher16.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        AbsoluteTime zeroTime = Clock.getRealtimeClock().getTime();
+        despotTrhead.start();
+       
+        th1.start();
+        try {
+            th1.join(60000);
+            
+            despotTrhead.setContinueExcecution(false);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Launcher24.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+
+        System.out.println("Laucher: Stampa Risultati");
+
+        Vector <Log> logs = new Vector<Log>();
+        logs.add(th1.getLog());
+        logs.add(handler.getLog());
+        String result =Util.relativeMerge(logs, zeroTime);
+        System.out.println(result);
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ SolarisNetBeansProjects/ProveJavaRealTime/src/realtimeLibrary/schedulables/ASAPStopThresholdPolicyHandler.java
@@ -0,0 +1,138 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package realtimeLibrary.schedulables;
+
+/**
+ *
+ * @author Marco Nanni
+ * classe che modella un  modella un gestore che, finchè un job non sfora
+ * consecutivamente la deadline più di un certo valore adotta la politica asap,
+ * schedulando i job successivi immediatamente dopo la fine del primo.
+ * Tuttavia, qualora il job sfori almeno tante deadline quante la soglia,
+ * allora si decide di interromperne l'esecuzione.
+ */
+public class ASAPStopThresholdPolicyHandler extends ThresholdPolicyHandler{
+
+    private ASAPPolicyHandler asapPolicyHandler;
+    private StopPolicyHandler stopPolicyHandler;
+    private int skipCount;
+
+    public ASAPStopThresholdPolicyHandler() {
+        super();
+        asapPolicyHandler = new ASAPPolicyHandler();
+        stopPolicyHandler = new StopPolicyHandler();
+    }
+
+    public ASAPStopThresholdPolicyHandler(InterrumpiblePeriodicThread controlledThread, int priority, String name, int threshold) {
+    super(controlledThread, priority, name, threshold);
+    asapPolicyHandler = new ASAPPolicyHandler(controlledThread, priority, name);
+    stopPolicyHandler = new StopPolicyHandler(controlledThread, priority, name);
+    this.getControlledThread().setPendingJobManager(this);
+    }
+
+     @Override
+    public void handleAsyncEvent() {
+        this.getLog().writeDeadlineMissed(this.getControlledThread().getName());
+         this.getControlledThread().setPendingMode(true);
+         this.incrementDeadlineCount();
+         if(this.getDeadlineCount()<this.getThreshold()){
+             asapPolicyHandler.handleAsyncEvent();
+         }
+     else{// ho raggiunto il limite: devo stoppare il thread
+             this.setSkipCount(this.getDeadlineCount()-1);
+             this.setDeadlineCount(0);
+             this.getAsapPolicyHandler().setPendingReleases(0);
+             this.getStopPolicyHandler().handleAsyncEvent();
+
+     }
+
+    }
+
+
+    @Override
+    public void doPendingJob(PeriodicThread managedThread) {
+       if (this.getSkipCount()==0){
+           this.getLog().writeStartPendingJob();
+           this.asapPolicyHandler.doPendingJob(this.getControlledThread());
+           this.getLog().writeEndPendingJob();
+       }
+     else{
+           this.getLog().writeSkippedJob();
+           this.decrementSkipCount();
+           if (this.getSkipCount()==0)
+               this.getControlledThread().setPendingMode(false);
+
+     }
+           
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public InterrumpiblePeriodicThread getControlledThread() {
+        return (InterrumpiblePeriodicThread) super.getControlledThread();
+    }
+
+  
+    public void setControlledThread(InterrumpiblePeriodicThread controlledThread) {
+        super.setControlledThread(controlledThread);
+    }
+
+
+
+
+
+
+
+
+    
+
+
+    protected ASAPPolicyHandler getAsapPolicyHandler() {
+        return asapPolicyHandler;
+    }
+
+    protected void setAsapPolicyHandler(ASAPPolicyHandler asapPolicyHandler) {
+        this.asapPolicyHandler = asapPolicyHandler;
+    }
+
+    protected StopPolicyHandler getStopPolicyHandler() {
+        return stopPolicyHandler;
+    }
+
+    protected void setStopPolicyHandler(StopPolicyHandler stopPolicyHandler) {
+        this.stopPolicyHandler = stopPolicyHandler;
+    }
+
+    protected int getSkipCount() {
+        return skipCount;
+    }
+
+    protected void setSkipCount(int skipCount) {
+        this.skipCount = skipCount;
+    }
+
+    protected void incrementSkipCount(){
+        this.skipCount++;
+    }
+
+    protected void decrementSkipCount(){
+        this.skipCount--;
+    }
+
+
+
+
+
+
+
+
+}
